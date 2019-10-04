cebf48c896f72df5d5bcb5905936eaa36f9caf83
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-04 09:54:49 -0600

    project endpoints completed

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -14,4 +14,52 @@ router.get('/', (req, res)=>{
     })
 })
 
+router.post('/', (req, res)=>{
+    const projectBody = req.body;
+    projectModel
+    .insert(projectBody)
+    .then(project=>{
+        res.status(201).json(project)
+    })
+    .catch(error=>{
+        res.status(500).json({ errorMessage: "Project could not be created."})
+    })
+})
+
+router.put('/:id', (req, res)=>{
+    const id = req.params.id;
+    const changes = req.body;
+    projectModel
+    .update(id, changes)
+    .then(project=>{
+        res.status(201).json(project)
+    })
+    .catch(error=>{
+        res.status(500).json({ errorMessage: "Project could not be updated."})
+    })
+})
+
+router.delete('/:id', (req, res)=>{
+    const id = req.params.id;
+    projectModel
+    .remove(id)
+    .then(project=>{
+        res.status(204).json(project)
+    })
+    .catch(error=>{
+        res.status(500).json({ errorMessage: "Project could not be removed."})
+    })
+})
+
+router.get('/:id', (req, res)=>{
+    const id = req.params.id;
+    projectModel
+    .getProjectActions(id)
+    .then(project=>{
+        res.status(200).json(project)
+    })
+    .catch(error=>{
+        res.status(500).json({ errorMessage: "Actions from project could not be returned."})
+    })
+})
 module.exports = router;
